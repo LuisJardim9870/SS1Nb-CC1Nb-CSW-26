6ea3f0a591e9513029f4892460acf131608c6ef3
Luis.Jardim <168872902+LuisJardim9870@users.noreply.github.com>
2026-05-13 22:36:30 -0300

    Add files via upload

---
+++ Aula-14/javascript.js
@@ -0,0 +1,17 @@
+var x = 10
+if (x > 5) {
+    console.log("x is greater than 5");
+} else if (x < 5) {
+    console.log("x is lees than 5");
+} else {
+    console.log("x is equal to 5"); 
+}
+
+for (let i = 0; i < 10; i++) {
+if (i % 2 === 0) {
+    console.log(i); 0, 2, 4, 6, 8
+};
+}
+
+for (let)
+
